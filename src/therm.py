7c3888a5b1ee975fdd44769220d00dc35c6cbd83
Zhai Jinpeng <42470911+Prethea-Phoenixia@users.noreply.github.com>
2023-07-10 17:44:59 +0800

    progress

--- src/therm.py
+++ src/therm.py
@@ -1,26 +1,6 @@
 import difflib
 
-# fmt: off
-#the E stand for electron and D for deuterium*/
-molarMasses = {
-  "H": 1.00794,   "HE": 4.002602,"LI": 6.941,    "BE": 9.012182,"B": 10.811,    "C": 12.0107,
-  "N": 14.00674,  "O": 15.9994,  "F": 18.9984032,"NE": 20.11797,"NA": 22.989770,"MG": 24.305,
-  "AL": 26.981538,"SI": 28.0855, "P": 30.973761, "S": 32.066,   "CL": 35.4527,  "AR": 39.948,
-  "K": 39.0983,   "CA": 40.078,  "SC": 44.95591, "TI": 47.88,   "V": 50.9415,   "CR": 51.996,
-  "MN": 54.938,   "FE": 55.847,  "CO": 58.9332,  "NI": 58.6934, "CU": 63.546,   "ZN": 65.39,
-  "GA": 69.723,   "GE": 72.61,   "AS": 74.9216,  "SE": 78.96,   "BR": 79.904,   "KR": 83.80,
-  "RB": 85.4678,  "SR": 87.62,   "Y": 88.9059,   "ZR": 91.224,  "NB": 92.9064,  "MO": 95.94,
-  "TC": 98.0,     "RU": 101.07,  "RH": 102.9055, "PD": 106.42,  "AG": 107.868,  "CD": 112.41,
-  "IN": 114.82,   "SN": 118.71,  "SB": 121.757,  "TE": 127.60,  "I": 126.9045,  "XE": 131.29,
-  "CS": 132.9054, "BA": 137.33,  "LA": 138.9055, "CE": 140.12,  "PR": 140.9077, "ND": 144.24,
-  "PM": 145.,     "SM": 150.36,  "EU": 151.965,  "GD": 157.25,  "TB": 158.9253, "DY": 162.50,
-  "HO": 164.9303, "ER": 167.26,  "TM": 168.9342, "YB": 173.04,  "LU": 174.967,  "HF": 178.49,
-  "TA": 180.9479, "W": 183.85,   "RE": 186.207,  "OS": 190.2,   "IR": 192.22,   "PT": 195.08,
-  "AU": 196.9665, "HG": 200.59,  "TL": 204.383,  "PB": 207.2,   "BI": 208.9804, "PO": 209.,
-  "AT": 210.,     "RN": 222.,    "FR": 223.,     "RA": 226.0254,"AC": 227.,     "TH": 232.0381,
-  "PA": 231.0359, "U": 238.029,  "NP": 237.0482, "PU": 244.,    "FM": 257.0,    "E": 0,
-  "D": 2};
-# fmt: on
+from hunt import balance
 
 
 class Ingredient:
@@ -110,6 +90,44 @@ class Ingredient:
             print('Unknown ingredient description "{:}"'.format(name) + "\n")
             return None
 
+
+"""
+(X): nbr. molecule per unit mass of propellant
+{Y}: nbr. atom per unit mass of propellant
+
+unless otherwise stated, mol/gram is assumed for
+propellant work derived form Hunt.
+
+Eq. 2.04
+(CO) * (H2O) / [(CO2) * (H2)] = K0
+
+Eq. 2.06
+(N2)                    = 0.5 * {N}
+(CO) + (CO2)            = {C}
+(H2) + (H2O)            = 0.5 * {H}
+(CO) + 2(CO2) + (H2O)   = {O}
+
+From Eq.2.06 and Eq.2.04
+Major                                   Minor
+(N2)    = 0.5  {N}                      | - 0.5 (N) - 0.5 (NO)
+(CO)    = {C} - (CO2)                   |
+(H2O)   = {O} - {C} - (CO2)             | - (OH) - (NO) - 2 [ (O2) + .5 (O) ]
+(H2)    = 0.5  {H} + {C} - {O} + (CO2)  | - 0.5 (H) + 2 (O2) + (o) + (NO) + 0.5 (OH)
+
+
+n = {C} + 0.5 * {H} + 0.5 * {N}         | + (OH) + (H) + (NO) + (O2) + (O) + (N)
+   CO/CO2     H2O/H2        N2
+
+dissociaiton considered ,in descending order of significance:
+
+2 H2O <-> 2 OH + H2             (OH)
+H2 <-> 2 H                      (H)
+2 H2O + N2 <-> 2 H2 + 2 NO      (NO)
+2 H2O <-> 2 H2 + O2             (O2)
+O2 <-> 2 O                      (O)
+N2 <-> 2 N                      (N)
+
+"""
 
 if __name__ == "__main__":
     Ingredient.readFile("data/PEPCODED.DAF")
